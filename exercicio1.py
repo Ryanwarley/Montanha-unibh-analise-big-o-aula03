7d2b9b8236d96a26c904d479f5735e50321930a8
RYAN WARLEY ALVES FERNANDES <e228124@gasmig.com.br>
2026-03-04 10:31:26 -0300

    Atualiza exercicio 1 - adiciona teste

--- exercicio1.py
+++ exercicio1.py
@@ -1,1 +1,7 @@
-print("Projeto Python funcionando!")
+def verificar_primeiro(lista):
+    if len(lista) == 0:
+        return None
+    return lista[0]
+
+# Teste
+print(verificar_primeiro([10, 20, 30]))
